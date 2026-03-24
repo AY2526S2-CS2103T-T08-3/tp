9bab7f82e4a15a6804bb0bdd201e7f430a9a3d9d
Jaris Chow <jaris2203@gmail.com>
2026-03-24 18:53:58 +0800

    Fix model execution for new AssignCommand

--- src/main/java/seedu/address/logic/commands/AssignCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignCommand.java
@@ -5,24 +5,59 @@ import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+import java.util.Arrays;
+
 import static java.util.Objects.requireNonNull;
+
+class Driver {
+
+}
 
 public class AssignCommand extends Command {
 
     public static final String COMMAND_WORD = "edit";
 
     public static final String MESSAGE_DUPLICATE_DRIVER = "There are duplicate drivers!";
+    public static final String MESSAGE_SUCCESS = "Drivers added and assigned successfully!";
+
+    private Driver[] drivers;
+
+    /**
+     * Creates an AssignCommand to tag all {@code Person}s to a {@code Driver}
+     */
+    public AssignCommand(Driver... inputDrivers) throws CommandException {
+        for (int i = 0; i < inputDrivers.length; i++) {
+            Driver toAdd = inputDrivers[i];
+            if (inputHasDuplicate(toAdd)) {
+                throw new CommandException(MESSAGE_DUPLICATE_DRIVER);
+            } else {
+                drivers[i] = toAdd;
+            }
+
+        }
+    }
+
+    private boolean inputHasDuplicate(Driver toAdd) {
+        for (Driver d : drivers) {
+            if (d.equals(toAdd)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        if (model.hasPerson(toAdd)) {
-            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        // TODO: UniqueDriverList in AddressBook with methods to check Drivers
+        if (model.hasDriver(drivers)) {
+            throw new CommandException(MESSAGE_DUPLICATE_DRIVER);
         }
 
-        model.addPerson(toAdd);
-        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
+        model.addDrivers(drivers);
+        return new CommandResult(MESSAGE_SUCCESS);
+
     }
 
     @Override
@@ -32,18 +67,18 @@ public class AssignCommand extends Command {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof AddCommand)) {
+        if (!(other instanceof AssignCommand)) {
             return false;
         }
 
-        AddCommand otherAddCommand = (AddCommand) other;
-        return toAdd.equals(otherAddCommand.toAdd);
+        AssignCommand otherAssignCommand = (AssignCommand) other;
+        return Arrays.equals(drivers, otherAssignCommand.drivers);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("toAdd", toAdd)
+                .add("toAssignDrivers", drivers)
                 .toString();
     }
 }

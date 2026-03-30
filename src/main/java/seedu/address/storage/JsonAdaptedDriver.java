34970d2619a1676c4cb35a63d78defe256ad7451
Jaris Chow <jaris2203@gmail.com>
2026-03-30 12:24:05 +0800

    Fix import order

--- src/main/java/seedu/address/storage/JsonAdaptedDriver.java
+++ src/main/java/seedu/address/storage/JsonAdaptedDriver.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.delivery.Driver;
 import seedu.address.model.commons.name.Name;
 import seedu.address.model.commons.phone.Phone;
+import seedu.address.model.delivery.Driver;
 
 /**
  * Jackson-friendly version of {@link Driver}.
